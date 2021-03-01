eeb653e71c39fa298c91e0c3d3483870e46ef0c0
jpgleeson <jack.p.gleeson@gmail.com>
2021-02-28 19:03:17 -0500

    FIX: Only sites owned by owner show up in index
    Forgot to check against ownerId when making query.

--- Pages/Tenants/Index.cshtml.cs
+++ Pages/Tenants/Index.cshtml.cs
@@ -7,23 +7,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using caint.Data;
 using caint.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace caintDashboard.Pages.Tenants
 {
-    public class IndexModel : PageModel
+    public class IndexModel : dashboard_BasePageModel
     {
-        private readonly caint.Data.caintDBContext _context;
 
-        public IndexModel(caint.Data.caintDBContext context)
+        public IndexModel(
+            caintDBContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
         public IList<Tenant> Tenant { get;set; }
 
         public async Task OnGetAsync()
         {
-            Tenant = await _context.tenants.ToListAsync();
+            var ownerId = UserManager.GetUserId(User);
+
+            Tenant = await _context.tenants.Where(x => x.ownerId == ownerId).ToListAsync();
         }
     }
 }
