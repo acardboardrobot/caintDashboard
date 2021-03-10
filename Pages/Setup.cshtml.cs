a5548e8e4584a9dc509c0d38570a529380875f0d
jpgleeson <jack.p.gleeson@gmail.com>
2021-03-10 14:43:17 -0500

    ADD: Setup instructions page. Assorted title fixes.
    Add setup page to the menu at the top with instructions on setting up caint.casa on a site.

---
+++ Pages/Setup.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace caintDashboard.Pages
+{
+    public class SetupModel : PageModel
+    {
+        private readonly ILogger<SetupModel> _logger;
+
+        public SetupModel(ILogger<SetupModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
