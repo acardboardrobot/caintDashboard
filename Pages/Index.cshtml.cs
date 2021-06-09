70b83d9dbfc997a4e27db2ee4d6ad2fedea511d9
Jack Gleeson <jack@jpgleeson.com>
2021-06-09 13:00:56 -0700

    Dashboard
    Dashboard now shows a list of the 3 latest comments and a count of unapproved comments.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,22 +6,40 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
+using caint.Data;
+using caint.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace caintDashboard.Pages
 {
     [AllowAnonymous]
-    public class IndexModel : PageModel
+    public class IndexModel : dashboard_BasePageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            caintDBContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _logger = logger;
         }
+
+        public IList<Comment> Comment { get;set; }
+        public int unapprovedCount;
 
         public void OnGet()
         {
+            var ownerId = UserManager.GetUserId(User);
+            Comment = _context.comments.Where(x => x.ownerId == ownerId).ToList();
 
+            unapprovedCount = _context.comments.Where(x => x.ownerId == ownerId && x.approved == false).ToList().Count;
+
+            if (Comment.Count > 3)
+            {
+                Comment = Comment.Skip(Comment.Count - 3).ToList();
+            }
         }
     }
 }
